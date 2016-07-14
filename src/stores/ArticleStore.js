6dd3547df5c1b30f6ad031b1958eb4f3ac0e64f3
m.ulyanov <m.ulyanov@live.ru>
2016-07-14 23:16:38 +0700

    Flux3: added BasicStore, refactored ArticleStore

--- src/stores/ArticleStore.js
+++ src/stores/ArticleStore.js
@@ -1,14 +1,11 @@
-import { EventEmitter } from 'events';
-import AppDispatcher from '../dispatcher';
 import { DELETE_ARTICLE } from '../constants'
+import BasicStore from './BasicStore'
 
-class ArticleStore extends EventEmitter {
-	constructor (initialState = []) {
-		super()
-		this._items = {}
-		initialState.forEach(this._add)
+class ArticleStore extends BasicStore {
+	constructor (...args) {
+		super(...args)
 
-		AppDispatcher.register((action) => {
+		this._registerActionSubscription((action) => {
 			const { type, payload } = action
 
 			switch (type) {
@@ -18,32 +15,6 @@ class ArticleStore extends EventEmitter {
 				break
 			}
 		})
-	}
-
-	getAll() {
-		return Object.keys(this._items).map(this.getById)
-	}
-
-	getById = (id) => this._items[id]
-
-	_delete(id) {
-		delete this._items[id]
-	}
-
-	_add = (item) => {
-		this._items[item.id] = item
-	}
-
-	addChangeListener(callback) {
-		this.on('SOME_INTERNAL_EVENT', callback);
-	}
-
-	removeChangeListener(callback) {
-		this.removeListener('SOME_INTERNAL_EVENT', callback);
-	}
-
-	emitChange() {
-		this.emit('SOME_INTERNAL_EVENT');
 	}
 }
 
